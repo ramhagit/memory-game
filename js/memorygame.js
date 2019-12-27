31a0ba1102966abe861c13597d88b903109d73d6
hagitram <ram1hagit@gmail.com>
2019-12-27 16:37:53 +0200

    basic card structure added to html and css

---
+++ js/memorygame.js
@@ -0,0 +1,23 @@
+{
+    class Game {
+        constructor(){
+            this.param1 = param1;
+        }
+    
+        someFunc(){
+            
+        }
+        
+    }
+
+    class Board {
+        constructor(){
+            this.rows = 3;
+            this.columns = 4;
+
+        }
+
+        clearBoard(){}
+    }
+    
+}
